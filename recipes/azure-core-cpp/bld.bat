9a6358055b7991760ba49a5aa562ca7d480bc490
John Blischak <jdblischak@gmail.com>
2023-07-05 15:53:11 -0400

    Change to azure-core subdirectory

--- recipes/azure-core-cpp/bld.bat
+++ recipes/azure-core-cpp/bld.bat
@@ -2,6 +2,8 @@
 
 set "AZURE_SDK_DISABLE_AUTO_VCPKG=ON"
 
+cd sdk\core\azure-core
+
 mkdir build
 cd build
 cmake %CMAKE_ARGS% ..
--- recipes/azure-core-cpp/build.sh
+++ recipes/azure-core-cpp/build.sh
@@ -6,6 +6,8 @@ set -ex
 # https://github.com/search?utf8=%E2%9C%93&q=AZURE_SDK_DISABLE_AUTO_VCPKG&type=code
 export AZURE_SDK_DISABLE_AUTO_VCPKG=ON
 
+cd sdk/core/azure-core
+
 # https://github.com/Azure/azure-sdk-for-cpp/blob/main/CONTRIBUTING.md#building-the-project
 mkdir build
 cd build
